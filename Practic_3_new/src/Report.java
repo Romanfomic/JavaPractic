37f39e7a9c612c9b63e10d33a32176be1743c715
Romanfomic <120161777+Romanfomic@users.noreply.github.com>
2022-12-09 09:28:32 +0300

    Delete Practic_3_new directory

--- Practic_3_new/src/Report.java
+++
@@ -1,7 +0,0 @@
-public class Report {
-    public static void generateReport(Employee[] array){
-        for(int i = 0; i < array.length; i++){
-            System.out.printf("%-15s %15s %n", array[i].getFullname(), array[i].getSalary());
-        }
-    }
-}
