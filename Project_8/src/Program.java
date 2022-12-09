756d370d7047d8294fca87c4ef3a7b17065e4f0f
Romanfomic <120161777+Romanfomic@users.noreply.github.com>
2022-12-09 09:37:02 +0300

    Delete Project_8 directory

--- Project_8/src/Program.java
+++
@@ -1,8 +0,0 @@
-public class Program
-{
-    public static void main(String[] args){
-        System.out.println(Recursion.Number_5(123));
-        System.out.println(Recursion.Number_10(123, 0));
-        Recursion.Number_15(123);
-    }
-}
